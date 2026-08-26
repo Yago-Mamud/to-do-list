411e214b0c8f3ba594db849d8e325ff6d00b4e48
Yago <yagomamud11@gmail.com>
2026-08-26 13:42:03 -0300

    can now create custom tasks

--- project/scripts.js
+++ project/scripts.js
@@ -1,8 +1,18 @@
 //elements
 const taskAdderButton = document.getElementById("taskAdder");
 const taskContainer = document.getElementById("taskContainer");
+const taskDiffTXT = document.getElementById("taskDiffTXT");
 //adder handler
 taskAdderButton.addEventListener("click", () => {
+	//gets current
+	const taskNameTXT = document.getElementById("taskNameTXT").value;
+	const taskDiffValue = taskDiffTXT.value;
+	const taskPointsValue = taskDiffTXT.options[taskDiffTXT.selectedIndex].getAttribute("data-points");
+	//validation
+	if (taskNameTXT == "" || taskDiffValue == "") {
+		alert("Please, fill out every field");
+		return false;
+	}
 	//new task main div
 	const newTask = document.createElement("div");
 	newTask.classList.add("task");
@@ -11,23 +21,23 @@ taskAdderButton.addEventListener("click", () => {
 		taskInfoContainer.classList.add("information");
 			//task name
 			const taskName = document.createElement("h1");
-			taskName.innerHTML = "testName";
+			taskName.innerHTML = taskNameTXT;
 			//task description container
 			const taskDesc = document.createElement("div");
 			taskDesc.classList.add("flexbox","description");
 				//task difficulty
 				const taskDiff = document.createElement("h2");
-				taskDiff.innerHTML = "testDiff";
+				taskDiff.innerHTML = taskDiffValue;
 				//task point amount
 				const taskPoints = document.createElement("h2");
-				taskPoints.innerHTML = "testPoints";
+				taskPoints.innerHTML = taskPointsValue;
 		//end task button
 		const taskEndButton = document.createElement("button");
 		taskEndButton.innerHTML = "END TASK"
 	//creates parent child relationship
-		newTask.append(taskInfoContainer, taskEndButton);
 		taskInfoContainer.append(taskName, taskDesc);
 		taskDesc.append(taskDiff, taskPoints);
+		newTask.append(taskInfoContainer, taskEndButton);
 	//inserts into html
 	taskContainer.appendChild(newTask);
 })
